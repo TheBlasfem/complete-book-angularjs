74da47393a26703f0cd9a0051033f4456761f6b2
Julio <ljuliom@gmail.com>
2014-08-24 12:51:19 -0500

    ng-switch

--- script.js
+++ script.js
@@ -1,17 +1,5 @@
 angular.module('myApp', [])
-    .directive('myDirective', function() {
-      return {
-        restrict: 'A',
-        scope: {
-          myUrl: '=someAttr',
-          myLinkText: '@'
-        },
-        template: '\
-          <div>\
-            <label>My Url Field:</label>\
-            <input type="text" ng-model="myUrl" />\
-            <a href="{{myUrl}}">{{myLinkText}}</a>\
-          </div>\
-        '
-      }
-    })
+
+.controller('SomeController', function($scope) {
+  $scope.person = {};
+});
