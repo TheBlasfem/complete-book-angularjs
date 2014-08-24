f58a5425c9baf85e27d30d2da30107a894690b44
Julio <ljuliom@gmail.com>
2014-08-24 11:35:03 -0500

    validtion forms

--- script.js
+++ script.js
@@ -1,12 +1,5 @@
 app = angular.module("Myapp", []);
 
 app.controller('ParentController', function($scope) {
-  $scope.person = {greeted: false};
-});
-
-app.controller('ChildController', function($scope) {
-  $scope.sayHello = function() {
-    $scope.person.name = "Ari Lerner";
-    $scope.person.greeted = true;
-  }
+	
 });
